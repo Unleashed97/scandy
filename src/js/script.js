e4304129c6b3d2af9a9ea0c2f1a17d6d82448afd
Nichkov Roman <nichkovroman@gmail.com>
2022-10-26 04:12:50 +0300

    feat: add section placement

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,37 @@
 import * as customFunctions from './modules/functions.js'
 
 // vendors
+const swiper1 = new Swiper('.swiper--1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next--1',
+        prevEl: '.swiper-button-prev--1',
+    },
+})
+
+const swiper2 = new Swiper('.swiper--2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next--2',
+        prevEl: '.swiper-button-prev--2',
+    },
+})
+const swiper3 = new Swiper('.swiper--3', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next--3',
+        prevEl: '.swiper-button-prev--3',
+    },
+})
